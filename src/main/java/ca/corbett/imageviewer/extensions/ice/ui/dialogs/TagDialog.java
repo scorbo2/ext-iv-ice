dbe4891feba46755626b1c02be8976b1b6acfef9
Steve Corbett <steve@corbett.ca>
2025-09-29 14:48:05 -0600

    Issue #7 - better keyboard navigation on tag edit dialog

--- src/main/java/ca/corbett/imageviewer/extensions/ice/ui/dialogs/TagDialog.java
+++ src/main/java/ca/corbett/imageviewer/extensions/ice/ui/dialogs/TagDialog.java
@@ -4,17 +4,26 @@ import ca.corbett.forms.Alignment;
 import ca.corbett.forms.FormPanel;
 import ca.corbett.forms.Margins;
 import ca.corbett.forms.fields.LongTextField;
+import ca.corbett.imageviewer.extensions.ImageViewerExtensionManager;
 import ca.corbett.imageviewer.extensions.ice.TagIndex;
 import ca.corbett.imageviewer.extensions.ice.TagList;
 import ca.corbett.imageviewer.ui.MainWindow;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
 import javax.swing.BorderFactory;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class TagDialog extends JDialog {
@@ -34,6 +43,7 @@ public class TagDialog extends JDialog {
         setLayout(new BorderLayout());
         add(buildFormPanel(), BorderLayout.CENTER);
         add(buildButtonPanel(), BorderLayout.SOUTH);
+        addKeyBindings();
     }
 
     private JPanel buildFormPanel() {
@@ -52,16 +62,7 @@ public class TagDialog extends JDialog {
 
         JButton button = new JButton("Save");
         button.setPreferredSize(new Dimension(90,23));
-        button.addActionListener(e -> {
-            TagList newList = TagList.of(textField.getText());
-            newList.setPersistenceFile(tagList.getPersistenceFile());
-            newList.addAll(tagList.getTags());
-            tagList = newList;
-            tagList.save();
-            TagIndex.getInstance().addOrUpdateEntry(imageFile, tagList.getPersistenceFile());
-            MainWindow.getInstance().reloadCurrentImage();
-            dispose();
-        });
+        button.addActionListener(e -> handleSave());
         buttonPanel.add(button);
 
         button = new JButton("Cancel");
@@ -73,5 +74,55 @@ public class TagDialog extends JDialog {
 
         buttonPanel.setBorder(BorderFactory.createRaisedBevelBorder());
         return buttonPanel;
+    }
+
+    private void handleSave() {
+        TagList newList = TagList.of(textField.getText());
+        newList.setPersistenceFile(tagList.getPersistenceFile());
+        //newList.addAll(tagList.getTags()); // wtf? why add the original tags instead of just using the text field?
+        tagList = newList;
+        tagList.save();
+        TagIndex.getInstance().addOrUpdateEntry(imageFile, tagList.getPersistenceFile());
+        ImageViewerExtensionManager.getInstance().imageSelected(MainWindow.getInstance().getSelectedImage());
+        dispose();
+    }
+
+    private void addKeyBindings() {
+        // Modify our text area so that "enter" key triggers the dialog save:
+        InputMap im = textField.getTextArea().getInputMap(JComponent.WHEN_FOCUSED);
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "save");
+        ActionMap am = textField.getTextArea().getActionMap();
+        am.put("save", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleSave();
+            }
+        });
+
+        // Now get the root pane's input map for WHEN_IN_FOCUSED_WINDOW condition
+        // This allows the bindings to work in theory regardless of which component has focus
+        // (though I note the JTextArea still steals "enter", hence the code block above)
+        JRootPane rootPane = getRootPane();
+        InputMap inputMap = rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = rootPane.getActionMap();
+
+        // Bind Enter key to save handler:
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "ok");
+        actionMap.put("ok", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleSave();
+            }
+        });
+
+        // Bind ESC key to Cancel action - this works even if focus is in JTextArea,
+        // because JTextArea doesn't have any particular built-in handler for the escape key:
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "cancel");
+        actionMap.put("cancel", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
     }
 }
