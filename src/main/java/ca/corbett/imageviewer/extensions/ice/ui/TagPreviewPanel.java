e1dbfbfefac79d63cd8e5841a86b5357545e65f8
Steve Corbett <steve@corbett.ca>
2025-10-01 17:19:56 -0600

    Issue #17 - make tag preview panel clickable

--- src/main/java/ca/corbett/imageviewer/extensions/ice/ui/TagPreviewPanel.java
+++ src/main/java/ca/corbett/imageviewer/extensions/ice/ui/TagPreviewPanel.java
@@ -5,9 +5,16 @@ import ca.corbett.forms.FormPanel;
 import ca.corbett.forms.Margins;
 import ca.corbett.forms.fields.LongTextField;
 import ca.corbett.imageviewer.extensions.ice.TagList;
+import ca.corbett.imageviewer.extensions.ice.actions.TagSingleImageAction;
+import ca.corbett.imageviewer.ui.ImageInstance;
+import ca.corbett.imageviewer.ui.MainWindow;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Represents a read-only panel for displaying tags for the current image.
@@ -15,16 +22,27 @@ import java.awt.BorderLayout;
  * @author <a href="https://github.com/scorbo2">scorbo2</a>
  */
 public class TagPreviewPanel extends JPanel {
-    private LongTextField textField;
+    private final LongTextField textField;
 
     public TagPreviewPanel() {
         setLayout(new BorderLayout());
-        FormPanel formPanel = new FormPanel(Alignment.TOP_CENTER);
+        FormPanel formPanel = new FormPanel(Alignment.TOP_LEFT);
         formPanel.setBorderMargin(6);
         textField = LongTextField.ofDynamicSizingMultiLine("", 1);
-        textField.setMargins(new Margins(24, 4, 24, 4, 4));
+        textField.setMargins(new Margins(10, 4, 10, 4, 0));
         textField.setEnabled(false);
+        this.setBackground(Color.GREEN);
+        textField.getTextArea().setColumns(10); // dumb! remove once swing-extras #119 fix is available in 2.5 release
         textField.setHelpText("Read-only display. Press Ctrl+G to edit tags.");
+        textField.getTextArea().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ImageInstance currentImage = MainWindow.getInstance().getSelectedImage();
+                if (! currentImage.isEmpty()) {
+                    new TagSingleImageAction().actionPerformed(null);
+                }
+            }
+        });
         formPanel.add(textField);
         add(formPanel, BorderLayout.CENTER);
     }
