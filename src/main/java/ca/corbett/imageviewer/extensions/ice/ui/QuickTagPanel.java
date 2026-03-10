367322ffb4f1ff3b80da64d6c7c00c21959c35aa
Steve Corbett <steve@corbett.ca>
2026-03-10 17:58:18 -0600

    Remove unused import

--- src/main/java/ca/corbett/imageviewer/extensions/ice/ui/QuickTagPanel.java
+++ src/main/java/ca/corbett/imageviewer/extensions/ice/ui/QuickTagPanel.java
@@ -1,7 +1,6 @@
 package ca.corbett.imageviewer.extensions.ice.ui;
 
 import ca.corbett.extras.EnhancedAction;
-import ca.corbett.extras.ScrollUtil;
 import ca.corbett.extras.LookAndFeelManager;
 import ca.corbett.extras.ScrollUtil;
 import ca.corbett.extras.TextInputDialog;
